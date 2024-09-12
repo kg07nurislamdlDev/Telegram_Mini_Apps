634f51e1291d51a9a3ee441c33b4f373ec7fe012
kg07nurislam Dev <kg07nurislam.dev@gmail.com>
2024-09-12 21:25:15 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,11 @@
 const { Telegraf } = require('telegraf');
-const bot = new Telegraf('YOUR_BOT_API_TOKEN');
+const bot = new Telegraf('7277101188:AAHki3jY1q6oXIMaIekdtUvOTOcgpP20RH0');
 
 bot.start((ctx) => {
     ctx.reply('Welcome to Mini App Store! Click the button below to view apps.', {
         reply_markup: {
             inline_keyboard: [
-                [{ text: 'View Apps', web_app: { url: 'https://your-web-app-url.com' } }]
+                [{ text: 'View Apps', web_app: { url: 'https://kg07nurislamdldev.github.io/Telegram_Mini_Apps' } }]
             ]
         }
     });
